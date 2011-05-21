1e8ad245a4589c435240181f7fed7056608491cf
kchizhov <kchizhov@mirantis.com>
2011-05-21 23:39:24 +0400

    AVR atomics

--- mcucpp/AVR/atomic.h
+++ mcucpp/AVR/atomic.h
@@ -1,21 +1,67 @@
 #pragma once
 
-class DisableInterrupts
+namespace Atomic
 {
-public:
-	DisableInterrupts()
+	class DisableInterrupts
 	{
-		_sreg = SREG;
-		asm("cli");
-	}
-	~DisableInterrupts()
-	{
-		SREG = _sreg;
-	}
-	operator bool()
-	{return false;}
-private:
-	uint8_t _sreg;
-};
+	public:
+		DisableInterrupts()
+		{
+			_sreg = SREG;
+			asm("cli");
+		}
+		~DisableInterrupts()
+		{
+			SREG = _sreg;
+		}
+		operator bool()
+		{return false;}
+	private:
+		uint8_t _sreg;
+	};
 
-#define ATOMIC if(DisableInterrupts di = DisableInterrupts()){}else
+#define ATOMIC if(Atomic::DisableInterrupts di = Atomic::DisableInterrupts()){}else
+
+#define DECLARE_OP(OPERATION, OP_NAME) \
+	template<class T, class T2>\
+	T FetchAnd ## OP_NAME (volatile T * ptr, T2 value)\
+	{\
+		ATOMIC \
+		{\
+			T tmp = *ptr;\
+			*ptr = tmp OPERATION value;\
+			return tmp;\
+		}\
+	}\
+	template<class T, class T2>\
+	T OP_NAME ## AndFetch(volatile T * ptr, T2 value)\
+	{\
+		ATOMIC \
+		{\
+			T tmp = *ptr OPERATION value;\
+			*ptr = tmp;\
+			return tmp;\
+		}\
+	}
+
+
+	DECLARE_OP(+, Add)
+	DECLARE_OP(-, Sub)
+	DECLARE_OP(|, Or)
+	DECLARE_OP(&, And)
+	DECLARE_OP(^, Xor)
+
+
+	template<class T, class T2>
+	bool CompareExchange(T * ptr, T2 oldValue, T2 newValue)
+	{
+		ATOMIC 
+		{
+			if(*ptr != oldValue)
+				return false;
+			*ptr = newValue;
+		}
+		return true;
+	}
+
+}
