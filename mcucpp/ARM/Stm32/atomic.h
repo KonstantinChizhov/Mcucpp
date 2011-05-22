f9aaa3439a3885c50873c6454f9d5065e125e369
kchizhov <kchizhov@mirantis.com>
2011-05-22 12:17:28 +0400

    atomics draft for STM32

---
+++ mcucpp/ARM/Stm32/atomic.h
@@ -0,0 +1,66 @@
+#pragma once
+#include <core_cm3.h>
+
+namespace Atomic
+{
+	class DisableInterrupts
+	{
+	public:
+		DisableInterrupts()
+		{
+			_sreg = __get_PRIMASK();
+			__disable_irq()
+		}
+		~DisableInterrupts()
+		{
+			__set_PRIMASK(_sreg);
+		}
+		operator bool()
+		{return false;}
+	private:
+		uint32_t _sreg;
+	};
+
+#define ATOMIC if(Atomic::DisableInterrupts di = Atomic::DisableInterrupts()){}else
+
+#define DECLARE_OP(OPERATION, OP_NAME) \
+	template<class T, class T2>\
+	T FetchAnd ## OP_NAME (volatile T * ptr, T2 value)\
+	{\
+		ATOMIC \
+		{\
+			T tmp = *ptr;\
+			*ptr = tmp OPERATION value;\
+			return tmp;\
+		}\
+	}\
+	template<class T, class T2>\
+	T OP_NAME ## AndFetch(volatile T * ptr, T2 value)\
+	{\
+		ATOMIC \
+		{\
+			T tmp = *ptr OPERATION value;\
+			*ptr = tmp;\
+			return tmp;\
+		}\
+	}
+
+	DECLARE_OP(+, Add)
+	DECLARE_OP(-, Sub)
+	DECLARE_OP(|, Or)
+	DECLARE_OP(&, And)
+	DECLARE_OP(^, Xor)
+
+	template<class T, class T2>
+	bool CompareExchange(T * ptr, T2 oldValue, T2 newValue)
+	{
+		ATOMIC 
+		{
+			if(*ptr != oldValue)
+				return false;
+			*ptr = newValue;
+		}
+		return true;
+	}
+
+}
