e829314e878eba4414fd9a8be1699299706aba0a
Konstantin Chizhov <kchizhov@mirantis.com>
2012-03-16 12:48:16 +0400

    KS0108 driver and a few of graphics support

---
+++ mcucpp/graphics/bitmap.h
@@ -0,0 +1,80 @@
+
+#ifndef __MCUCPP_BITMAP_H__
+#define __MCUCPP_BITMAP_H__
+
+namespace Graphics
+{
+	template<class PtrT = unsigned char*>
+	class BitRef
+	{
+	public:
+		BitRef(PtrT byte, unsigned char mask)
+		:_byte(byte), _mask(mask)
+		{}
+		operator unsigned char ()
+		{
+			return (*_byte & _mask) != 0;
+		}
+		BitRef &operator =(unsigned char value)
+		{
+			if(value)
+				*_byte |= _mask;
+			else
+				*_byte &= ~_mask;
+			return *this;
+		}
+	private:
+		PtrT _byte;
+		unsigned char _mask;
+	};
+
+	template<class PtrT = unsigned char*>
+	class BitmapBase
+	{	
+	public:
+		BitmapBase(PtrT data, unsigned width, unsigned height)
+		:_data(data), _dim1(width), _dim2(height)
+		{
+			_bytesPerLine = (_dim1 + 7) >> 3;
+		}
+	
+		BitRef<PtrT> operator()(unsigned x, unsigned y)
+		{
+			return BitRef<PtrT>(_data + y * _bytesPerLine + (x >> 3), 1 << (x & 7));
+		}
+	
+		unsigned Width(){return _dim1;};
+		unsigned Height(){return _dim2;};
+
+	protected:
+		PtrT _data;
+		unsigned _dim1, _dim2, _bytesPerLine;
+	};
+
+	typedef BitmapBase<unsigned char*> Bitmap;
+
+
+	template<class PtrT = unsigned char*>
+	class ColumnOrderedBitmap
+	{
+	public:
+		ColumnOrderedBitmap(PtrT data, unsigned width, unsigned height)
+		:_data(data), _dim1(width), _dim2(height)
+		{
+			_bytesPerLine = (_dim2 + 7) >> 3;
+		}
+	
+		BitRef<PtrT> operator()(unsigned x, unsigned y)
+		{
+			return BitRef<PtrT>(_data + x * _bytesPerLine + (y >> 3), 1 << (y & 7));
+		}
+	
+		unsigned Width(){return _dim1;};
+		unsigned Height(){return _dim2;};
+
+	protected:
+		PtrT _data;
+		unsigned _dim1, _dim2, _bytesPerLine;
+	};
+}
+#endif
---
+++ mcucpp/graphics/font.h
@@ -0,0 +1,35 @@
+#ifndef __MCUCPP_FONT_H__
+#define __MCUCPP_FONT_H__
+
+#include <graphics/bitmap.h>
+
+namespace Graphics
+{
+	template<class BytePtrT>
+	class RastrFont
+	{
+	public:
+		typedef ColumnOrderedBitmap<BytePtrT> CharType;
+		RastrFont(BytePtrT fontData, unsigned charWidth, unsigned charHeight);
+		ColumnOrderedBitmap<BytePtrT> GetChar(char c);
+	protected:
+		BytePtrT _data;
+		unsigned _width, _height;
+	};
+
+	template<class BytePtrT>
+	RastrFont<BytePtrT>::RastrFont(BytePtrT fontData, unsigned charWidth, unsigned charHeight)
+	:_data(fontData), _width(charWidth), _height(charHeight)
+	{
+		
+	}
+	
+	template<class BytePtrT>
+	ColumnOrderedBitmap<BytePtrT> RastrFont<BytePtrT>::GetChar(char c)
+	{
+		unsigned offset = ((_height + 7) >> 3) * _width * (c - 32);
+		return ColumnOrderedBitmap<BytePtrT>(_data + offset, _width, _height);
+	}
+}
+
+#endif
