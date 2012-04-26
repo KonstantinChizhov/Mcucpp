fdf79acc2fbc41c43b173779a27bc21b5953d3e8
Konstantin Chizhov <kchizhov@mirantis.com>
2012-04-26 20:08:41 +0400

    AVR watchdog

---
+++ mcucpp/AVR/watchdog.h
@@ -0,0 +1,70 @@
+
+//*****************************************************************************
+//
+// Author		: Konstantin Chizhov
+// Date			: 2012
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without modification, 
+// are permitted provided that the following conditions are met:
+// Redistributions of source code must retain the above copyright notice, 
+// this list of conditions and the following disclaimer.
+
+// Redistributions in binary form must reproduce the above copyright notice, 
+// this list of conditions and the following disclaimer in the documentation and/or 
+// other materials provided with the distribution.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+// EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//*****************************************************************************
+
+#pragma once
+#include <atomic.h>
+
+namespace Mcucpp
+{
+	class Watchdog
+	{
+		public:
+			enum Period
+			{
+				Period15ms = 0,
+				Period30ms = 1 << WDP0,
+				Period60ms = 1 << WDP1,
+				Period120ms = (1 << WDP0) | (1 << WDP1),
+				Period250ms = (1 << WDP2),
+				Period500ms = (1 << WDP2) | (1 << WDP0),
+				Period1s = (1 << WDP2) | (1 << WDP1),
+				Period2s = (1 << WDP0) | (1 << WDP1) | (1 << WDP0),
+			};
+			
+			static void Start(Period period)
+			{
+				WDTCR = period | (1 << WDE);
+				Reset();
+			}
+			
+			static void Disable()
+			{
+				ATOMIC
+				{
+					Reset();
+					WDTCR |= (1 << WDTOE) | (1 << WDE);
+					WDTCR = 0;
+				}
+			}
+			
+			static void Reset()
+			{
+				asm("wdr");
+			}
+	};
+}
