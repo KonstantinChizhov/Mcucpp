cfb68136c4c31e59af6ff6593c8574a7865b3e5e
Konstantin Chizhov <kchizhov@mirantis.com>
2016-09-19 16:00:51 +0300

    Fix Hosted target when running tests outside main repository

--- scons/hosted.py
+++ scons/hosted.py
@@ -9,8 +9,8 @@ def builder_unit_test(target, source, env):
 	for src in source:
 		objects.append(env.Object('%s-%s' % (targetName, os.path.splitext(str(src))[0]), src))
 	
-	objects.append(env.Object('%s-%s' % (targetName, 'gtest_main'), '#/gtest/gtest_main.cc'))
-	objects.append(env.Object('%s-%s' % (targetName, 'gtest-all'), '#/gtest/gtest-all.cc'))
+	objects.append(env.Object('%s-%s' % (targetName, 'gtest_main'), '%s/gtest/gtest_main.cc' % env['MCUCPP_HOME']))
+	objects.append(env.Object('%s-%s' % (targetName, 'gtest-all'), '%s/gtest/gtest-all.cc' % env['MCUCPP_HOME']))
 	testExeNode = env.Program('%s_test' % targetName, objects)
 	testApp = str(testExeNode[0].abspath)
 	testNode = env.Command('run_test_%s' % targetName, '', testApp)
@@ -25,12 +25,19 @@ def generate(env, **kw):
 	#else:
 	env.Tool('default')
 	
-	env.Append(CPPPATH = ['#/mcucpp', '#/gtest', '#/mcucpp/Test', '#/./'])
+	print 'Used C comiler "%s"' % env['CC']
+	
+	env.Append(CPPPATH = [\
+		'%s' % env['MCUCPP_HOME'],
+		'%s/mcucpp' % env['MCUCPP_HOME'], 
+		'%s/gtest' % env['MCUCPP_HOME'], 
+		'%s/mcucpp/Test' % env['MCUCPP_HOME'], 
+		'#/./'])
 	
 	if env['CC'] == 'cl':
 		env.Append(CXXFLAGS = ['/wd4503', '/wd4530'])
 		env.Append(CPPDEFINES=['_VARIADIC_MAX=10'])
-		env.Append(CPPPATH=['#/tests/include'])
+		env.Append(CPPPATH=['%s/tests/include' % env['MCUCPP_HOME']])
 		
 	testBuilder = Builder(action = builder_unit_test)
 	env.Append(BUILDERS = {'Test' :  testBuilder})
